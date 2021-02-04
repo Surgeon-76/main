21b65fbc9565f8dc4312ce3f66cd4466bd6a09ed
Surgeon-76 <Surgeon76@yandex.ru>
2021-02-04 13:41:15 +0300

    descrube

--- index.php
+++ index.php
@@ -11,10 +11,26 @@ if ($conn->connect_error) die("Fatal Error");
     PRIMARY KEY (id)
 )";
 */
-
+$query = "DESCRIBE cats";
 $result = $conn->query($query);
-if (!$result) die ("Сбой при доступе к базе данных");
+if (!$result) die ("Сбой при доступе к базе данных: " . $conn->error);
+$rows = $result->num_rows;
+echo "<table><tr> <th>Column</th> <th>Type</th>
+<th>Null</th> <th>Key</th> </tr>";
+for ($j = 0 ; $j < $rows ; ++$j)
+{
+$result->data_seek($j);
+$row = $result->fetch_arrow(MYSQLI_NUM);
+echo "<tr>";
+for ($k = 0 ; $k < 4 ; ++$k) echo "<td>$row[$k]</td>";
+echo "</tr>";
+}
+echo>
+"</table>";
 
+/*$result = $conn->query($query);
+if (!$result) die ("Сбой при доступе к базе данных");
+*/
 
 phpinfo();
 ?>
