404af329e4357ec7c0c3e1bb51a24445bebc71e6
Surgeon-76 <Surgeon76@yandex.ru>
2021-02-04 14:26:35 +0300

    real

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
-if ($conn->connect_error) die("Fatal Error");
+if ($conn->connect_error) die($conn->connect_error);
 
 /*$query = "CREATE TABLE cats (
     id SMALLINT NOT NULL AUTO_INCREMENT,
@@ -15,22 +15,20 @@ $query = "DESCRIBE cats";
 $result = $conn->query($query);
 if (!$result) die ("Сбой при доступе к базе данных: " . $conn->error);
 $rows = $result->num_rows;
-echo "<table><tr> <th>Column</th> <th>Type</th>
-<th>Null</th> <th>Key</th> </tr>";
+echo "<table><tr> <th>Column</th> <th>Type</th> <th>Null</th> <th>Key</th> </tr>";
 for ($j = 0 ; $j < $rows ; ++$j)
 {
 $result->data_seek($j);
-$row = $result->fetch_arrow(MYSQLI_NUM);
+$row = $result->fetch_array(MYSQLI_NUM);
 echo "<tr>";
 for ($k = 0 ; $k < 4 ; ++$k) echo "<td>$row[$k]</td>";
 echo "</tr>";
 }
-echo
-"</table>";
+echo "</table>";
 
 /*$result = $conn->query($query);
 if (!$result) die ("Сбой при доступе к базе данных");
 */
 
-phpinfo();
+// phpinfo();
 ?>
